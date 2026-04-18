58ece386fdc4468a8f3388515d90b93068b4280e
James Mathai <jammathai@gmail.com>
2026-04-18 17:53:08 -0400

    Add state containers and effects

--- homely.js
+++ homely.js
@@ -55,6 +55,92 @@ function appendText(element, string) {
   }
 }
 
+export function wrap(object) {
+  const refs = Object.fromEntries(
+    Object.entries(object).map(([key, value]) => [key, new Ref(value)]),
+  );
+
+  return new Proxy(refs, {
+    get(target, key) {
+      if (key[0] === "$") {
+        const value = target[key.slice(1)];
+        if (value === undefined) {
+          warn(`{ ... }.${key} is undefined`);
+        }
+        return value;
+      }
+      if (target[key]) {
+        return target[key].getValue();
+      }
+      return undefined;
+    },
+
+    set(target, key, value) {
+      if (key[0] === "$") {
+        error(
+          TypeError,
+          `{ ... }.${key} cannot be set because it is a state reference`,
+        );
+      }
+      if (!target[key]) {
+        target[key] = new Ref(value);
+        return true;
+      }
+      target[key].setValue(value);
+      return true;
+    },
+  });
+}
+
+export function addEffect(effect, deps) {
+  if (typeof effect !== "function") {
+    error(TypeError, "effect is not a function");
+  }
+
+  if (!Array.isArray(deps)) {
+    error(TypeError, "deps is not an array");
+  }
+
+  for (const [index, dep] of deps.entries()) {
+    if (!(dep instanceof Ref)) {
+      error(TypeError, `deps[${index}] is not a state reference`);
+    }
+    if (!dep.addEffect(effect)) {
+      warn(`effect has already been added to deps[${index}]`);
+    }
+  }
+  effect();
+}
+
+class Ref {
+  #value;
+  #effects;
+
+  constructor(value) {
+    this.#value = value;
+    this.#effects = [];
+  }
+
+  addEffect(effect) {
+    if (this.#effects.includes(effect)) {
+      return false;
+    }
+    this.#effects.push(effect);
+    return true;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  setValue(value) {
+    this.#value = value;
+    for (const effect of this.#effects) {
+      effect();
+    }
+  }
+}
+
 function consoleValue(value) {
   if (typeof value === "string") return `"${value}"`;
   return value;
@@ -62,6 +148,10 @@ function consoleValue(value) {
 
 function warn(...data) {
   console.warn(`(${getCallSite()})`, ...data);
+}
+
+function error(constructor, message) {
+  throw new constructor(`(${getCallSite()}) ${message}`);
 }
 
 function getCallSite() {
